4a12bb06076f81ec5e144ab42c9d9e5bd0067796
Lyle Xu <chanxl009@gmail.com>
2023-09-15 19:42:08 +0800

    run task after provisioning

--- Customized/main.ps1
+++ Customized/main.ps1
@@ -1,8 +1,23 @@
 param (
     [Parameter()]
-    [string]$LocalRepoURL
+    [string]$LocalRepoURL,
+    [Parameter()]
+    [string]$RunAsUser
 )
 
-winget source remove -n=winget
-winget source remove -n=msstore
-winget source add --name WinGetRest $LocalRepoURL -t Microsoft.Rest
+if($RunAsUser -eq "true") {
+    $CustomizationScriptsDir = "C:\DevBoxCustomizations"
+    $RunAsUserScript = "runAsUser.ps1"
+    $RunAsUserScriptPath = "$($CustomizationScriptsDir)\$($RunAsUserScript)"
+    if(!(Test-Path -Path $CustomizationScriptsDir)){
+        New-Item $CustomizationScriptsDir -type directory
+    }
+    Add-Content -Path $RunAsUserScriptPath -Value "winget source remove -n=winget
+    winget source remove -n=msstore
+    winget source add --name WinGetRest $LocalRepoURL -t Microsoft.Rest"
+    
+}else{
+    winget source remove -n=winget
+    winget source remove -n=msstore
+    winget source add --name WinGetRest $LocalRepoURL -t Microsoft.Rest 
+}
